29b3c652670775a945a141d00982a2018b937d7f
zykt <zyktnn@gmail.com>
2016-11-01 01:01:14 +0300

    Started TinyDB integration

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from bottle import Bottle, template, static_file, run
+from tinydb import TinyDB
+
+
+def create_band(name, description):
+    return {
+        'name': name,
+        'description': description,
+    }
 
 
 app = Bottle()
@@ -18,6 +26,9 @@ def band_page(band_name):
 @app.route('/static/<file_name>')
 def static(file_name):
     return static_file(filename=file_name, root=r'C:\Users\ian\PycharmProjects\metal_bottle\static')
+
+
+db = TinyDB('db/db.json')
 
 
 run(app, host='localhost', port=8080)
