9dce5529974274604f26e69539a0bc788d31d0f1
zykt <zyktnn@gmail.com>
2016-11-01 02:31:03 +0300

    Reformatted code a little

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from bottle import Bottle, template, static_file, run
 from tinydb import TinyDB
 
 
+app = Bottle()
+
+db = TinyDB('db/db.json')
+
+
 def create_band(name, description):
     return {
         'name': name,
         'description': description,
     }
-
-
-app = Bottle()
 
 
 @app.route('/')
@@ -26,9 +28,6 @@ def band_page(band_name):
 @app.route('/static/<file_name>')
 def static(file_name):
     return static_file(filename=file_name, root=r'C:\Users\ian\PycharmProjects\metal_bottle\static')
-
-
-db = TinyDB('db/db.json')
 
 
 run(app, host='localhost', port=8080)
